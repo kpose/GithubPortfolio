ab5947b36439e84c0a1f87358fe35732f4db921a
kpose <ganihujude@gmail.com>
2020-06-22 11:12:21 +0100

    serve and deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import Header from '../src/components/Header'
-import './App.css';
-import Profile from './containers/Profile'
+import React, { Component } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Profile from './containers/Profile';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Profile />
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppWrapper>
+          <Header />
+          <Profile />
+        </AppWrapper>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import './Header.css';
-import Link from './Link';
-import logo from '../logo.svg';
+import styled from 'styled-components';
+import logo from '../../src/GitHub-Mark-Light-64px.png';
 
+const HeaderWrapper = styled.div`
+  background-color: #282c34;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
-const Header =() => (
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link url='https://reactjs.org' title='Learn React' />
-      </header>
+const Logo = styled.img`
+  height: 64px;
+  pointer-events: none;
+`;
+
+const Header = () => (
+  <HeaderWrapper>
+    <Logo src={logo} alt='logo' />
+    <h1>Kpose's Github Portfolio</h1>
+  </HeaderWrapper>
 );
 
 export default Header;
